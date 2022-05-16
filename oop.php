213980cb03de2e4954867a0d107acaa7ae665eb7
M J <69307802+whitehat-1@users.noreply.github.com>
2022-05-16 05:00:55 -0700

    Update oop.php

--- oop.php
+++ oop.php
@@ -119,4 +119,16 @@ $p1-> hello() -> register() -> mail();
 // private: only accessible within the class
 // public: accessible from anywhere
 
- 
+class Car {
+    // Public methods and properties
+    public $model;
+    public function getModel()
+    {
+    return "The car model is " . $this -> model;
+    }
+    }
+    $mercedes = new Car();
+    // Here we access a property from outside the class
+    $mercedes -> model = "Mercedes";
+    // Here again we access another method from outside the class
+    echo $mercedes -> getModel();
