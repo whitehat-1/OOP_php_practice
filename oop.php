05b18bd1a8255edd0114f18ec20386a9e8fea997
M J <69307802+whitehat-1@users.noreply.github.com>
2022-05-16 05:03:26 -0700

    Update oop.php

--- oop.php
+++ oop.php
@@ -119,6 +119,7 @@ $p1-> hello() -> register() -> mail();
 // private: only accessible within the class
 // public: accessible from anywhere
 
+##Example1:
 class Car {
     // Public methods and properties
     public $model;
@@ -132,3 +133,19 @@ class Car {
     $mercedes -> model = "Mercedes";
     // Here again we access another method from outside the class
     echo $mercedes -> getModel();
+
+##Example2:
+    // private: only accessible within the class
+    class Car {
+        // Private
+        private $model;
+        public function getModel()
+        {
+        return "The car model is " . $this -> model;
+        }
+        }
+        $mercedes = new Car();
+        // We try to access a private property from outside the class.
+        $mercedes -> model = "Mercedes";
+        echo $mercedes -> getModel();
+// this result gives us an error!!!        
