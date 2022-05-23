44589bf160aa441f544df2e6ccab0a111ae80162
M J <69307802+whitehat-1@users.noreply.github.com>
2022-05-23 04:55:54 -0700

    Update magicmethods&constants.php

--- magicmethods&constants.php
+++ magicmethods&constants.php
@@ -1,2 +1,20 @@
 <?php
 
+//The__construct()magicmethod 
+// We use __construct() in order to do something as soon as we create an object out of a class.
+// In this case, we use it to set the default value of the $model property.
+
+class Car{
+    private $model;
+    // Constructor method://pass the value of the variable $model to the __construct() method
+    public function  __construct($model){
+        $this -> model =$model;
+    }
+    public function getCarModel()
+{
+    return $this -> model;
+ }
+}
+//pass the value of the variable after the object is created
+$car1 = new Car("Mercedes");
+echo $car1 -> getCarModel();
